1a658f6593cd1e286b5f6499f17fbb0819114dc9
Antrussian <antonino.russo.agent@gmail.com>
2023-11-27 11:36:54 +0100

    Creazione Array

---
+++ snacks-1/js/script.js
@@ -0,0 +1,24 @@
+/*
+Crea un array composto da 10 automobili.
+Ogni oggetto automobile avrà le seguenti proprietà: marca, modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
+
+Dividi le automobili in 3 array separati: nel primo array solo le auto a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
+ 
+Infine stampa separatamente i 3 array.
+
+*/
+
+const automobili = [
+    { marca: 'Toyota', modello: 'Corolla', alimentazione: 'benzina' },
+    { marca: 'Ford', modello: 'Focus', alimentazione: 'diesel' },
+    { marca: 'Tesla', modello: 'Model 3', alimentazione: 'elettrico' },
+    { marca: 'Fiat', modello: '500', alimentazione: 'gpl' },
+    { marca: 'Volkswagen', modello: 'Golf', alimentazione: 'benzina' },
+    { marca: 'Renault', modello: 'Clio', alimentazione: 'diesel' },
+    { marca: 'Nissan', modello: 'Leaf', alimentazione: 'elettrico' },
+    { marca: 'Opel', modello: 'Corsa', alimentazione: 'metano' },
+    { marca: 'Mercedes', modello: 'Classe-A', alimentazione: 'diesel' },
+    { marca: 'Chevrolet', modello: 'Bolt', alimentazione: 'elettrico' }
+  ];
+  
+  console.log(automobili);
